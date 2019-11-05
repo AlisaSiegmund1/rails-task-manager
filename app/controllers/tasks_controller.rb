aa417f1279500a19b10475e5a46d4752fed56c3f
Alisa Siegmund <alisa.siegmund@gmail.com>
2019-11-05 18:43:16 +0100

    fixed the models and everything

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,50 @@
 class TasksController < ApplicationController
+  # GET /tasks
+  def index
+    @tasks = Task.all
+  end
+
+  # GET /task/:id
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  # GET /task/new
+  def new
+    @task = Task.new
+  end
+
+  # POST /tasks
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # to save the instance of a new Restaurant
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params_plus)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require('task').permit(:title, :details)
+  end
+
+  def task_params_plus
+    params.require('task').permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'task/:id/edit', to: 'tasks#edit', as: :edit
+  get 'task/:id', to: 'tasks#show', as: :task
+  delete 'task/:id', to: 'tasks#destroy'
+  patch 'task/:id', to: 'tasks#update'
+  post 'tasks', to: 'tasks#create'
 end
